70160ed8d7406a8e73a242accde36faaf059d813
zziro95 <opers7878@gmail.com>
2023-05-02 14:35:56 +0900

    feat: Color Set 추가

---
+++ WonderLand/UIs/Styles/Color/Color+Extension.swift
@@ -0,0 +1,37 @@
+//
+//  Color+Extension.swift
+//  WonderLand
+//
+//  Created by Jinho Choi on 2023/05/02.
+//
+
+import SwiftUI
+
+extension Color {
+
+    // MARK: Primary
+
+    static let primary50 = Color("Primary50")
+    static let primary100 = Color("Primary100")
+    static let primary200 = Color("Primary200")
+    static let primary300 = Color("Primary300")
+    static let primary400 = Color("Primary400")
+    static let primary500 = Color("Primary500")
+    static let primary600 = Color("Primary600")
+    static let primary700 = Color("Primary700")
+    static let primary800 = Color("Primary800")
+    static let primary900 = Color("Primary900")
+
+    // MARK: Gray
+
+    static let gray50 = Color("Gray50")
+    static let gray100 = Color("Gray100")
+    static let gray200 = Color("Gray200")
+    static let gray300 = Color("Gray300")
+    static let gray400 = Color("Gray400")
+    static let gray500 = Color("Gray500")
+    static let gray600 = Color("Gray600")
+    static let gray700 = Color("Gray700")
+    static let gray800 = Color("Gray800")
+    static let gray900 = Color("Gray900")
+}
